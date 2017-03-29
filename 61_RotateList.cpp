13c60b391e04a13f7f15f309899da772a5ab4ffe
RheinX <747972298@qq.com>
2017-03-29 15:22:01 +0800

    correct a mistake

--- 61_RotateList.cpp
+++ 61_RotateList.cpp
@@ -1,45 +1,34 @@
+//descruption:
+//Given a list, rotate the list to the right by k places, where k is non - negative.
+//For example :
+//Given 1->2->3->4->5->NULL and k = 2,
+//return 4->5->1->2->3->NULL.
+
+//Solution
+//此题的k会超出链表长度，因此首先循环链表到尾部，得到链表长度和尾指针tail，tail->next = head, 
+//将其变成循环链表。利用k %= length，处理k大于length的情况，之后循环链表即可。
+
+
 #include <iostream>
 using namespace std;
 
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
 	ListNode* rotateRight(ListNode* head, int k) {
-		////方法一
-		//if (0 == k||NULL==head||NULL==head->next)
-		//	return head;
-
-		//ListNode* p = new ListNode(0), *next_p = head;
-		//p->next = head;
-
-		//for (int i = 0; i < k - 1; i++)
-		//	next_p = next_p->next;
-
-		//while (NULL!=next_p->next){
-		//	p = p->next;
-		//	next_p = next_p->next;
-		//}
-
-		//ListNode* answer = p->next;
-
-		//p->next = NULL;
-		//next_p->next = head;
-
-		//return answer;
-
 		//方法二
 		if (NULL == head || NULL == head->next)
 			return head;
 
 		int length = 1;
 		ListNode* p = head;
-		
-		while (p->next){
+
+		while (p->next) {
 			p = p->next;
 			length++;
 		}
@@ -50,7 +39,7 @@ public:
 				p = p->next;
 			}
 		}
-		
+
 		ListNode* answer = p->next;
 		p->next = NULL;
 
@@ -63,11 +52,11 @@ int main() {
 	ListNode* l1 = new ListNode(1);
 	ListNode* l2 = new ListNode(2);
 	l1->next = l2;
-	
+
 	Solution a;
 	ListNode *answer = a.rotateRight(l1, 2);
 
-	while (answer->next){
+	while (answer->next) {
 		cout << answer->val << " ";
 		answer = answer->next;
 	}
